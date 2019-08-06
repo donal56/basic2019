a403ac0eb2d1fd5c4d7fceebd50c96ae0c4b311e
Liz Soveranez <elizabeth050698@gmail.com>
2019-08-06 14:03:43 -0500

    Revert "req"
    This reverts commit 1282d4a272e9f6ba04e2ed847d56c7e1b16f27e9.

--- views/req-requisicion/_form.php
+++ views/req-requisicion/_form.php
@@ -2,91 +2,35 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
-use yii\grid\GridView;
-use yii\helpers\ArrayHelper;
-use unclead\multipleinput\MultipleInput;
-use yii\db\Query;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Requisicion */
 /* @var $form yii\widgets\ActiveForm */
-
- $query = new Query;
-    $query  ->select([
-        'req_area.are_id as ID' , 
-        'CONCAT(req_personal.per_nombre, " ", req_personal.per_paterno, " ", req_personal.per_materno) as Nombre ']
-        )  
-        ->from('req_area')
-        ->join('INNER JOIN', 'req_personal',
-            'req_area.are_fkper_responsable = req_personal.per_id');
-
-    $command = $query->createCommand();
-    $data1 = $command->queryAll();
-
-    $data2 = $query ->where(['req_area.are_nivel' => '2'])->createCommand()->queryAll();
-
-    $data3 = $query ->where(['req_area.are_nivel' => '1'])
-        ->createCommand()->queryAll();
-
-    $data4 = $query ->where(['req_area.are_nivel' => '0'])
-        ->createCommand()->queryAll();;
-
-    $query = new Query;
-    $data5 = $query->select(['con_id as ID' , 'CONCAT("INSTITUTO TECNOLÓGICO DE ", con_instituto) as Instituto'])->from('req_configuracion')->createCommand()->queryAll();
-
-
 ?>
 
 <div class="requisicion-form">
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form -> field($model, 'req_fecha') -> input('date', ['value' => date('Y-m-d'), ]); ?>
-    <?= $form -> field($model, 'req_folio'); ?>
-    <?= $form -> field($model, 'req_fkper_solicitante') -> dropDownList(ArrayHelper::map($data1, "ID", "Nombre")) ?>
-    <?= $form -> field($model, 'req_fechaSolicitante') -> input('date', ['value' => date('Y-m-d'), ]); ?>
-    <?= $form -> field($model, 'req_esoperativo') -> checkbox(['label' => '¿Es parte del presupuesto operativo anual?']); ?>
-    <?= $form -> field($model, 'req_justificacion') -> textarea(['label' => 'Justificación', 'rows' => 5]); ?>
-    <?= $form -> field($model, 'req_fkper_subdirector') -> dropDownList(ArrayHelper::map($data2, "ID", "Nombre")) ?>
-    <?= $form -> field($model, 'req_fkper_planeacion') -> dropDownList(ArrayHelper::map($data3, "ID", "Nombre")) ?>
-    <?= $form -> field($model, 'req_fkper_director') -> dropDownList(ArrayHelper::map($data4, "ID", "Nombre")) ?>
-    <?= $form -> field($model, 'req_fkconfiguracion') -> dropDownList(ArrayHelper::map($data5, "ID", "Instituto")) ?>
+    <?= $form->field($model, 'req_fecha')->textInput() ?>
 
-    <?= $form->field($modelDet, 'det_id')->widget(MultipleInput::className(), 
-    [
-        'allowEmptyList'    => false,
-        'addButtonPosition' => MultipleInput::POS_ROW,
-        'prepend'   => true,
-        'sortable' => true,
-        'columns' => 
-        [
-            [
-            'name'  => 'det_clave',
-            'title' => 'Clave'
-            ],
-            [
-            'name'  => 'det_partida',
-            'title' => 'Partida'
-            ],
-            [
-            'name'  => 'req_cantidad',
-            'title' => 'Cantidad'
-            ],
-            [
-            'name'  => 'req_unidad',
-            'title' => 'Unidad'
-            ],
-            [
-            'name'  => 'req_descripcion',
-            'title' => 'Descripcion'
-            ],
-            [
-            'name'  => 'req_costo',
-            'title' => 'Costo'
-            ]
-        ]
-    ]);
-    ?>
+    <?= $form->field($model, 'req_folio')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'req_fkper_solicitante')->textInput() ?>
+
+    <?= $form->field($model, 'req_fechasolicitante')->textInput() ?>
+
+    <?= $form->field($model, 'req_esoperativo')->textInput() ?>
+
+    <?= $form->field($model, 'req_justificacion')->textarea(['rows' => 6]) ?>
+
+    <?= $form->field($model, 'req_fkper_subdirector')->textInput() ?>
+
+    <?= $form->field($model, 'req_fkper_planeacion')->textInput() ?>
+
+    <?= $form->field($model, 'req_fkper_director')->textInput() ?>
+
+    <?= $form->field($model, 'req_fkconfiguracion')->textInput() ?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
