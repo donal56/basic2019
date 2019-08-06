c460d1ffd2f5289911e7ef9c399ed2ec93c9bda2
Sergio Daniel Jimenez Zcarias <hmaforsiv@gmail.com>
2019-08-06 13:07:38 -0500

    added db

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=sia2019.heliohost.org;dbname=sia2019_req',
+    'username' => 'sia2019_root',
+    'password' => 'intellideskSIA',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
